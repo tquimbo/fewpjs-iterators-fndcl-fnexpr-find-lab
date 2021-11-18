ce201e95350597285c08d4ebf94087258a58cecf
Tim Quimbo <tquimbo1@gmail.com>
2021-11-17 19:30:23 -0500

    commit tree 05a62fd92c77f1013bbfd6e494429a4e06694e51

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+function superbowlWin(arr) {
+    const won = arr.find(game => game.result === "W" )
+    if (won) { return won.year }
+  }
